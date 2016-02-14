749960a1f298a19462cb46cdc5a22f3d6887b576
Knut Hühne <knut@k-nut.eu>
2016-02-14 17:03:21 +0100

    Add initital validation file
    This script can be called from the command line. It returns all the
    geojson files that have additional, unneccessary properties.

---
+++ validate.js
@@ -0,0 +1,44 @@
+var fs = require('fs'),
+    fsTools = require('fs-tools'),
+    _ = require('lodash')
+
+var EXPECTED = ['cartodb_id', 'created_at', 'name', 'updated_at'];
+
+
+function getExpectedKeys(geoJsonFilePath){
+  var metaDataPath = geoJsonFilePath.replace('geojson', 'metadata.json');
+  var data = JSON.parse(fs.readFileSync(metaDataPath, 'utf8'));
+  var languages = _.values(data.languages);
+  return _.uniq(EXPECTED.concat(languages)).sort();
+}
+
+function validateFile(filePath){
+  var expectedKeys = getExpectedKeys(filePath);
+
+  var data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  var oldKeys = [];
+  var keys;
+  data.features.forEach(function(feature){
+    keys = Object.keys(feature.properties);
+    if (!_.isEqual(keys, oldKeys)){
+      oldKeys = keys;
+      oldKeys = oldKeys.sort();
+    }
+  });
+  return _.difference(oldKeys, expectedKeys);
+}
+
+
+fsTools.findSorted('public/data', /[^.]+\.geojson/, function(err, files) {
+  var fullPath;
+  files.forEach(function(filename){
+    if (_.includes(filename, '_work-in-progress')){
+      return;
+    }
+    fullPath =  __dirname + '/' + filename
+    var difference = validateFile(fullPath);
+    if (difference.length){
+      console.log(filename, 'contains additional fields: ', difference);
+    }
+  });
+});
